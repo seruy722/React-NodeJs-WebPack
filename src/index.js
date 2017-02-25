170ce530134b6098ac7bdc276777977045584ab5
sergey <seruy722@gmail.com>
2017-02-26 01:35:54 +0200

    start aplication

---
+++ src/index.js
@@ -0,0 +1,23 @@
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+let mountNode = document.getElementById('app');
+
+
+class App extends React.Component {
+  render() {
+    let arr = [1,2,3];
+    return (
+      <div>
+      <h1> My first React Component {2+2}</h1>
+      {arr.map( number =>{
+        return <h2>Hello {number}</h2>
+      })}
+      <h2>Hello</h2>
+      </div>
+      )
+  }
+}
+
+ReactDOM.render(<App />, mountNode);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  entry: ['./src/index.js'],
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015', 'stage-1']
+        }
+      }, {
+        test: /\.css$/,
+        loader: 'style-loader!css-loader!postcss-loader'
+      }
+    ]
+  },
+  plugins: [],
+  devServer: {
+    historyApiFallback: true,
+    contentBase: './',
+    inline: true,
+    port: 3000
+  }
+};
+
+if (process.env.NODE_ENV == 'production') {
+  module.exports.plugins.push(
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      }
+    })
+  )
+}
